99b280cc2ba271dcff560a76ed0c80afc992e43e
FrankYeah <aaa24295234@gmail.com>
2021-09-14 00:07:49 +0800

    img

---
+++ pages/about.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="about">
+    
+  </div>
+</template>
+
+<script>
+
+export default {
+  head: {
+    title: '關於',
+  },
+  layout: 'default',
+  components: {
+
+  },
+  props: {
+
+  },
+  data () {
+    return {
+
+    }
+  },
+  mounted () {
+    
+  },
+  computed: {
+
+  },
+  methods: {
+    
+  },
+  watch: {
+    
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+.about {
+
+}
+
+@media( max-width: 500px ){
+
+}
+
+</style>
